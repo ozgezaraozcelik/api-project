9f874b592510996d3ecb38bb810067339fe1b6d1
OZGE ZARA OZCELIK <37712@aksa.com>
2026-04-07 09:51:09 +0300

    Personel Egitim API projesi yuklemesi

---
+++ Controllers/TrainingsController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PersonnelTrainingApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TrainingsController : ControllerBase
+    {
+        /// <summary>
+        /// Tüm eğitim programlarını listeler.
+        /// </summary>
+        [HttpGet]
+        public IActionResult Get() => Ok("Eğitim listesi hazır!");
+
+        /// <summary>
+        /// Yeni bir eğitim tanımlar.
+        /// </summary>
+        [HttpPost]
+        public IActionResult Post() => Ok("Eğitim başarıyla eklendi.");
+    }
+}
